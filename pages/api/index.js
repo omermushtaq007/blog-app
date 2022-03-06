f82f6b3c03b4d6154c2f9662e79eedb092540072
umer mushtaq <umermushtaq3424@gmail.com>
2022-03-06 18:19:54 +0500

    connect with database and connection tested successfully!

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dbConnect from '../../lib/dbConnect';
+import User from '../../models/User';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+export default async function handler(req, res) {
+  const { method } = req;
+  await dbConnect();
+  switch (method) {
+    case 'GET':
+      return getUsers(req, res);
+    case 'POST':
+      return createUser(req, res);
+  }
 }
+
+const getUsers = async (req, res) => {
+  const users = await User.find();
+  res.status(200).json(users);
+};
+const createUser = async (req, res) => {};
