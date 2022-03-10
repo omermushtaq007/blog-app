248feecc8d1bf5f15b56cfb450f55012409a43cd
umer mushtaq <umermushtaq3424@gmail.com>
2022-03-10 23:55:07 +0500

    new things

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,26 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Stack } from 'react-bootstrap';
 
 export default function Hero() {
   return (
-    <Container>
-      <Row>
-      </Row>
+    <Container className="pt-3  overflow-hidden">
+      <Stack
+        direction="horizontal"
+        className="bg-primary overflow-hidden"
+        gap={1}
+      >
+        <div className="bg-secondary col-6">danger</div>
+        <div className="bg-light col-3">danger</div>
+        <div className="bg-light col-3">danger</div>
+      </Stack>
+      <Stack
+        direction="horizontal"
+        className="bg-primary pt-1 overflow-hidden"
+        gap={1}
+      >
+        <div className="bg-secondary col-3">danger</div>
+        <div className="bg-light col-3">danger</div>
+        <div className="bg-light col-6">danger</div>
+      </Stack>
     </Container>
   );
 }
--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -1,42 +1,34 @@
 import Image from 'next/image';
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown, Stack } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, NavDropdown, Stack } from 'react-bootstrap';
 
 export default function TopBar() {
   return (
-    <Navbar bg="secondary" expand="lg">
+    <Navbar bg="light" className="border-2 border-bottom" expand="lg">
       <Container>
         {/* <Stack direction="horizontal" gap={3}> */}
         <Navbar.Brand className="d-flex gap-2">
           <Image src="/earth.svg" alt="" width="50" height="50" />
-          <span className="text-light fs-2 ml-2 align-items-center">
+          <span className="text-dark fs-2 ml-2 align-items-center">
             <strong>Posting Home</strong>
           </span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="/icons/bars-solid.svg" />
         {/* </Stack> */}
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Link href="#home">
-              <Image
-                src="/icons/house-solid.svg"
-                alt=""
-                width="20"
-                height="20"
-              />
-            </Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto gap-lg-3">
+            {/* Home */}
+            <Nav.Link href="#home">Home</Nav.Link>
+            {/* Link */}
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+            <div className="vr" />
+            <Button variant="light" onClick={() => console.log(`Login`)}>
+              Login
+            </Button>
+            <Button variant="primary" onClick={() => console.log(`Register`)}>
+              Register
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
